0b1bb0bed1698f208f4cb8fd4e0ddd438e596c8a
Alexis Tyler <xo@wvvw.me>
2015-08-30 14:51:40 +0930

    Moved files back into main dir

--- old/assets/php/system_load_ajax.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<?php
-	Ini_Set( 'display_errors', true );
-	include '../../init.php';
-	include ROOT_DIR . '/assets/php/functions.php';
-
- 	makeLoadBars();
-  ?>
--- old/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('ROOT_DIR', dirname(__FILE__));
-define('ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(ROOT_DIR))));
-?>
